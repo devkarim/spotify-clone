fefe037c68d7d1dec01e6369c3d4770d98439f90
Karim <devkarim@hotmail.com>
2023-08-09 22:08:07 +0300

    Add font similar to what's being used in spotify

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import './globals.css';
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] });
+import type { Metadata } from 'next';
+import { Figtree } from 'next/font/google';
+
+const font = Figtree({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Spotify - Listen to Music wherever you are',
@@ -16,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={font.className}>{children}</body>
     </html>
   );
 }
