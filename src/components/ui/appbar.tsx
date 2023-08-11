8f2c82d52f33f2d650578cf09aa81c070880c749
Karim <devkarim@hotmail.com>
2023-08-11 19:28:19 +0300

    Add premium link in appbar

--- src/components/ui/appbar.tsx
+++ src/components/ui/appbar.tsx
@@ -26,6 +26,13 @@ const Appbar: React.FC<AppbarProps> = ({}) => {
         </button>
       </div>
       <div className="flex gap-8 items-center">
+        <Link
+          href="/premium"
+          className="font-bold opacity-60 hover:opacity-100"
+        >
+          Premium
+        </Link>
+        <div className="divider divider-vertical before:bg-white after:bg-white m-0 h-8" />
         <Link href="/signup" className="font-bold opacity-60 hover:opacity-100">
           Sign up
         </Link>
