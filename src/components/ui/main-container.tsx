48aab7404c3773e53d068e691e9ff5e2172d4f99
Karim <devkarim@hotmail.com>
2023-08-14 00:29:26 +0300

    Make main container include basic classes

--- src/components/ui/main-container.tsx
+++ src/components/ui/main-container.tsx
@@ -1,13 +1,16 @@
 'use client';
 
-import useScroll from '@/hooks/use-scroll';
 import { DetailedHTMLProps, HTMLAttributes } from 'react';
+
+import { cn } from '@/lib/utils';
+import useScroll from '@/hooks/use-scroll';
 
 interface MainContainerProps
   extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {}
 
 const MainContainer: React.FC<MainContainerProps> = ({
   children,
+  className,
   ...props
 }) => {
   const scroll = useScroll();
@@ -15,6 +18,10 @@ const MainContainer: React.FC<MainContainerProps> = ({
   return (
     <div
       onScroll={(e) => scroll.setOffset(e.currentTarget.scrollTop)}
+      className={cn(
+        'bg-backgroundSecondary h-full w-full overflow-y-auto bg-no-repeat bg-local',
+        className
+      )}
       {...props}
     >
       {children}
