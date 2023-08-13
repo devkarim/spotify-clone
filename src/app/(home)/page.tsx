6e17e7f0f81f883243238842bb183a82ca9b6527
Karim <devkarim@hotmail.com>
2023-08-13 23:04:38 +0300

    Add more sections in home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -11,9 +11,25 @@ const HomePage: React.FC<HomePageProps> = async ({}) => {
   const isSignedIn = user?.isAuthenticated;
 
   return (
-    <div className="p-6 pt-0">
+    <div className="p-6 pt-0 space-y-8">
       <Section
         title="Recently Played"
+        items={playlists.map((p) => ({
+          title: p.name,
+          subtitle: 'Playlist - 0 songs',
+          imageUrl: p.imageUrl,
+        }))}
+      />
+      <Section
+        title="New picks"
+        items={playlists.map((p) => ({
+          title: p.name,
+          subtitle: 'Playlist - 0 songs',
+          imageUrl: p.imageUrl,
+        }))}
+      />
+      <Section
+        title="Welcome to 2023!"
         items={playlists.map((p) => ({
           title: p.name,
           subtitle: 'Playlist - 0 songs',
