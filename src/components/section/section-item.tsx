0ab058bbfb91e27e9c84c0ff9c73f27009166e45
Karim <devkarim@hotmail.com>
2023-08-13 23:05:04 +0300

    Add rounded option in section item component

--- src/components/section/section-item.tsx
+++ src/components/section/section-item.tsx
@@ -1,21 +1,28 @@
 import Image from 'next/image';
 
 import PlayButton from '../ui/play-button';
+import { cn } from '@/lib/utils';
 
 interface SectionItemProps {
   title: string;
   subtitle: string;
   imageUrl: string;
+  rounded?: boolean;
 }
 
 const SectionItem: React.FC<SectionItemProps> = ({
   title,
   subtitle,
   imageUrl,
+  rounded = false,
 }) => {
   return (
     <div className="group bg-section-card p-6 rounded-md space-y-2 cursor-pointer hover:bg-section-card-secondary transition-colors">
-      <div className="relative h-40 w-40 rounded-md overflow-hidden">
+      <div
+        className={cn('relative h-40 w-40 rounded-md overflow-hidden', {
+          'rounded-full': rounded,
+        })}
+      >
         <Image src={imageUrl} alt="section-item-image" fill />
         <PlayButton className="absolute bottom-0 right-0" />
       </div>
