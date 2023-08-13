346b2e690b944c8de3984d2cc829c39b7a184d5e
Karim <devkarim@hotmail.com>
2023-08-13 17:06:48 +0300

    Fix home playlist cards being aligned improperly on mobile

--- src/components/playlist/home-playlists.tsx
+++ src/components/playlist/home-playlists.tsx
@@ -7,7 +7,7 @@ interface HomePlaylistsProps {
 
 const HomePlaylists: React.FC<HomePlaylistsProps> = ({ playlists = [] }) => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+    <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-6">
       {playlists.map((p) => (
         <PlaylistHomeCard key={p.name} name={p.name} imageUrl={p.imageUrl} />
       ))}
