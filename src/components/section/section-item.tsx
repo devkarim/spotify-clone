73e8e6838cad3798bedfd93432ecac7194da078b
Karim <devkarim@hotmail.com>
2023-08-14 00:32:00 +0300

    Make section item responsive

--- src/components/section/section-item.tsx
+++ src/components/section/section-item.tsx
@@ -17,13 +17,21 @@ const SectionItem: React.FC<SectionItemProps> = ({
   rounded = false,
 }) => {
   return (
-    <div className="group bg-section-card p-6 rounded-md space-y-2 cursor-pointer hover:bg-section-card-secondary transition-colors">
+    <div className="group bg-section-card w-full lg:w-fit p-6 rounded-md space-y-2 cursor-pointer hover:bg-section-card-secondary transition-colors">
       <div
-        className={cn('relative h-40 w-40 rounded-md overflow-hidden', {
-          'rounded-full': rounded,
-        })}
+        className={cn(
+          'relative h-40 w-full lg:w-40 rounded-md overflow-hidden',
+          {
+            'rounded-full': rounded,
+          }
+        )}
       >
-        <Image src={imageUrl} alt="section-item-image" fill />
+        <Image
+          src={imageUrl}
+          alt="section-item-image"
+          className="object-cover"
+          fill
+        />
         <PlayButton className="absolute bottom-0 right-0" />
       </div>
       <h3 className="font-bold text-lg">{title}</h3>
