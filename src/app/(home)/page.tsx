042ef1858f6d6505d6be743a5344463a3dda9daa
Karim <devkarim@hotmail.com>
2023-08-15 23:42:26 +0300

    Add header for home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,6 +1,8 @@
-import Section from '@/components/section/main-section';
-import { getUserPlaylists } from '@/services/server/playlist';
 import { getUser } from '@/services/server/session';
+import Section from '@/components/section/main-section';
+import WelcomeHeader from '@/components/ui/welcome-header';
+import { getUserPlaylists } from '@/services/server/playlist';
+import HomePlaylists from '@/components/playlist/home-playlists';
 
 interface HomePageProps {}
 
@@ -11,38 +13,46 @@ const HomePage: React.FC<HomePageProps> = async ({}) => {
   const isSignedIn = user?.isAuthenticated;
 
   return (
-    <div className="p-6 pt-0 space-y-8">
+    <>
       {isSignedIn && (
-        <Section
-          title="Recently Played"
-          items={playlists.map((p) => ({
-            title: p.name,
-            subtitle: 'Playlist - 0 songs',
-            imageUrl: p.imageUrl,
-          }))}
-        />
+        <div className="p-6 space-y-6">
+          <WelcomeHeader />
+          <HomePlaylists playlists={playlists} />
+        </div>
       )}
-      {isSignedIn && (
-        <Section
-          title="New picks"
-          items={playlists.map((p) => ({
-            title: p.name,
-            subtitle: 'Playlist - 0 songs',
-            imageUrl: p.imageUrl,
-          }))}
-        />
-      )}
-      {isSignedIn && (
-        <Section
-          title="Welcome to 2023!"
-          items={playlists.map((p) => ({
-            title: p.name,
-            subtitle: 'Playlist - 0 songs',
-            imageUrl: p.imageUrl,
-          }))}
-        />
-      )}
-    </div>
+      <div className="p-6 pt-0 space-y-8">
+        {isSignedIn && (
+          <Section
+            title="Recently Played"
+            items={playlists.map((p) => ({
+              title: p.name,
+              subtitle: 'Playlist - 0 songs',
+              imageUrl: p.imageUrl,
+            }))}
+          />
+        )}
+        {isSignedIn && (
+          <Section
+            title="New picks"
+            items={playlists.map((p) => ({
+              title: p.name,
+              subtitle: 'Playlist - 0 songs',
+              imageUrl: p.imageUrl,
+            }))}
+          />
+        )}
+        {isSignedIn && (
+          <Section
+            title="Welcome to 2023!"
+            items={playlists.map((p) => ({
+              title: p.name,
+              subtitle: 'Playlist - 0 songs',
+              imageUrl: p.imageUrl,
+            }))}
+          />
+        )}
+      </div>
+    </>
   );
 };
 
