e75115d8ce58733ae0a9c43e03acad2f2926f75e
Karim <devkarim@hotmail.com>
2023-08-16 23:45:33 +0300

    Make playlist image url optional
    Make playlist image optional and pass data schema directly to
    createPlaylist function

--- src/schemas/playlistSchema.ts
+++ src/schemas/playlistSchema.ts
@@ -4,9 +4,7 @@ export const playlistSchema = z.object({
   name: z
     .string({ required_error: 'Playlist name is required' })
     .min(1, 'Playlist name is required'),
-  imageUrl: z
-    .string({ required_error: 'Playlist image is required.' })
-    .url('Playlist image is invalid'),
+  imageUrl: z.string().url('Playlist image is invalid').optional(),
 });
 
 export type PlaylistSchema = z.infer<typeof playlistSchema>;
--- src/services/client/playlist.ts
+++ src/services/client/playlist.ts
@@ -1,11 +1,10 @@
 import { Playlist } from '@prisma/client';
 import { BaseResponse } from '@/types/api';
+import { PlaylistSchema } from '@/schemas/playlistSchema';
 
 import client from './axios';
 
 type PlaylistResponse = BaseResponse<Playlist>;
 
-export const createPlaylist = (name: string, imageUrl: string) =>
-  client
-    .post<PlaylistResponse>('/playlist', { name, imageUrl })
-    .then((res) => res.data.data);
+export const createPlaylist = (data: PlaylistSchema) =>
+  client.post<PlaylistResponse>('/playlist', data).then((res) => res.data.data);
