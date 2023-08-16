86b75e697ad08ab839ad4cf705ecbc48dc6be55c
Karim <devkarim@hotmail.com>
2023-08-16 23:52:52 +0300

    Add songbutton & playlist image to playlist header

--- src/app/playlist/[playlistId]/components/playlist-header.tsx
+++ src/app/playlist/[playlistId]/components/playlist-header.tsx
@@ -1,20 +1,29 @@
-import Image from 'next/image';
+import AddSongButton from '@/components/ui/add-song-button';
+import PlaylistImage from '@/components/playlist/playlist-image';
 
 interface PlaylistHeaderProps {
+  id: bigint;
   title: string;
-  imageUrl: string;
+  imageUrl: string | null;
 }
 
-const PlaylistHeader: React.FC<PlaylistHeaderProps> = ({ title, imageUrl }) => {
+const PlaylistHeader: React.FC<PlaylistHeaderProps> = ({
+  id,
+  title,
+  imageUrl,
+}) => {
   return (
-    <div className="flex gap-6 items-center">
-      <div className="relative h-72 w-72 rounded-md overflow-hidden">
-        <Image src={imageUrl} alt={title} fill className="object-cover" />
+    <div className="flex flex-col sm:flex-row gap-12 items-center justify-between">
+      <div className="flex flex-col sm:flex-row gap-6 items-center">
+        <div className="relative h-72 w-72 rounded-md overflow-hidden shadow-xl">
+          <PlaylistImage imageUrl={imageUrl} />
+        </div>
+        <div className="space-y-4">
+          <p>Playlist</p>
+          <h1 className="font-bold text-5xl sm:text-7xl">{title}</h1>
+        </div>
       </div>
-      <div className="space-y-4">
-        <p>Playlist</p>
-        <h1 className="font-bold text-5xl sm:text-7xl">{title}</h1>
-      </div>
+      <AddSongButton playlistId={id} />
     </div>
   );
 };
