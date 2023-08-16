8909c7654269d4fbff8c6488cc6cbed3051c64df
Karim <devkarim@hotmail.com>
2023-08-17 00:57:19 +0300

    Add play icon  beside song in songs table
    Add play icon visible on hover in songs list component

--- src/components/songs/songs-list.tsx
+++ src/components/songs/songs-list.tsx
@@ -4,6 +4,7 @@ import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 
 import { Song } from '@prisma/client';
+import { FaPlay } from 'react-icons/fa';
 
 interface SongsListProps {
   songs: Song[];
@@ -27,8 +28,16 @@ const SongsList: React.FC<SongsListProps> = ({ songs }) => {
           </thead>
           <tbody>
             {songs.map((song, index) => (
-              <tr key={song.id.toString()}>
-                <th>{index + 1}</th>
+              <tr key={song.id.toString()} className="group">
+                <th className="relative">
+                  <p className="group-hover:hidden">{index + 1}</p>
+                  <span
+                    className="absolute bottom-5 hidden group-hover:block tooltip tooltip-right"
+                    data-tooltip={`Play ${song.name}`}
+                  >
+                    <FaPlay />
+                  </span>
+                </th>
                 <td>{song.name}</td>
                 <td>{song.artist || 'n/a'}</td>
                 <td>{song.album || 'n/a'}</td>
