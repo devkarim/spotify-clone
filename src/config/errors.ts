bd4011d6a4cda577c4fc1b54ba8b5afba1ae6b99
Karim <devkarim@hotmail.com>
2023-08-16 23:28:58 +0300

    Add new error messages
    Add invalid playlist id and invalid file error messages

--- src/config/errors.ts
+++ src/config/errors.ts
@@ -5,8 +5,10 @@ const Errors = {
   invalidCredentials: ServerError.new(Messages.errors.invalidCredentials, 400),
   emailTaken: ServerError.new(Messages.errors.emailTaken, 400),
   unauthenticated: ServerError.new(Messages.errors.unauthenticated, 400),
+  invalidPlaylistId: ServerError.new(Messages.errors.invalidPlaylistId, 400),
   invalidId: ServerError.new(Messages.errors.invalidId, 400),
   notFound: ServerError.new(Messages.errors.notFound, 404),
+  invalidFile: ServerError.new(Messages.errors.invalidFile, 404),
 } as const;
 
 export default Errors;
--- src/config/messages.ts
+++ src/config/messages.ts
@@ -3,8 +3,10 @@ const Messages = {
     invalidCredentials: 'Invalid email or password.',
     emailTaken: 'Email is already taken.',
     unauthenticated: 'Unauthenticated.',
+    invalidPlaylistId: 'Invalid playlist id.',
     invalidId: 'Invalid id.',
     notFound: 'Not found.',
+    invalidFile: 'Invalid file.',
   },
 } as const;
 
