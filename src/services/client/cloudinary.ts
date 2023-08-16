8d8a590a85d455031ff99aa1c5f40260f8f007a6
Karim <devkarim@hotmail.com>
2023-08-16 23:40:01 +0300

    Validate file before uploading & add resource_type
    Validate file extension before uploading to cloudinary and add
    resource_type argument to choose preferred file extension

--- src/services/client/cloudinary.ts
+++ src/services/client/cloudinary.ts
@@ -4,22 +4,36 @@ import {
   CLOUDINARY_CLOUD_NAME,
   CLOUDINARY_PRESET_NAME,
 } from '@/config/constants';
+import Errors from '@/config/errors';
+import { validateFile } from '@/lib/utils';
+
+export type ResourceType = 'image' | 'raw' | 'video' | 'auto';
 
 const client = axios.create({
   baseURL: `https://api.cloudinary.com/v1_1/${CLOUDINARY_CLOUD_NAME}`,
   headers: {
     'X-Requested-With': 'XMLHttpRequest',
   },
-  timeout: 10000,
+  // timeout: 10000,
 });
 
-const upload = async (file: File): Promise<string> => {
+const upload = async (
+  file: File,
+  resource_type: ResourceType = 'auto'
+): Promise<string> => {
+  if (!validateFile(file, resource_type)) throw Errors.invalidFile;
   const formData = new FormData();
   formData.append('upload_preset', CLOUDINARY_PRESET_NAME);
+  formData.append('resource_type', resource_type);
   formData.append('file', file);
-  return client
-    .post('/image/upload', formData)
-    .then((res) => res.data.secure_url);
+  if (resource_type === 'video') return uploadVideo(formData);
+  return uploadImage(formData);
 };
+
+const uploadImage = (formData: FormData) =>
+  client.post('/image/upload', formData).then((res) => res.data.secure_url);
+
+const uploadVideo = (formData: FormData) =>
+  client.post(`/video/upload`, formData).then((res) => res.data.secure_url);
 
 export default upload;
