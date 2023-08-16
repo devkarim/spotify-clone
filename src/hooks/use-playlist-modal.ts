96566314aac4e4680d39569872c86679edd4e5a7
Karim <devkarim@hotmail.com>
2023-08-16 23:46:09 +0300

    Make use-playlist-modal hook import ModalStatus

--- src/hooks/use-playlist-modal.ts
+++ src/hooks/use-playlist-modal.ts
@@ -1,7 +1,7 @@
 import { create } from 'zustand';
 import { devtools } from 'zustand/middleware';
 
-type ModalStatus = 'create' | 'edit';
+import { ModalStatus } from '@/types/ui';
 
 interface PlaylistModalState {
   isOpen: boolean;
