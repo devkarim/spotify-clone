e7e252846bcf03b9fbc977dec9109e54b039231a
Karim <devkarim@hotmail.com>
2023-08-18 18:11:53 +0300

    feat: add player control component
    Add player control component that controls the web music player like
    playing/pausing current song and playing next/previous songs.

---
+++ src/components/player/player-control.tsx
@@ -0,0 +1,68 @@
+import { shallow } from 'zustand/shallow';
+import { useGlobalAudioPlayer } from 'react-use-audio-player';
+import { FaBackwardStep, FaForwardStep } from 'react-icons/fa6';
+
+import usePlayer from '@/hooks/use-player';
+import usePlaylist from '@/hooks/use-playlist';
+import PlayButton from '@/components/ui/play-button';
+
+interface PlayerControlProps {}
+
+const PlayerControl: React.FC<PlayerControlProps> = ({}) => {
+  const { song, playNextSong, playPrevSong } = usePlayer(
+    ({ song, playNextSong, playPrevSong }) => ({
+      song,
+      playNextSong,
+      playPrevSong,
+    }),
+    shallow
+  );
+  const playlist = usePlaylist((state) => state.playlist);
+  const { play, pause, playing, isLoading } = useGlobalAudioPlayer();
+
+  const playSong = () => {
+    if (!song) return;
+    if (playing) {
+      pause();
+    } else {
+      play();
+    }
+  };
+
+  const nextSong = () => {
+    if (!playlist) return;
+    playNextSong(playlist.songs);
+  };
+
+  const prevSong = () => {
+    if (!playlist) return;
+    playPrevSong(playlist.songs);
+  };
+
+  return (
+    <div className="flex items-center gap-6 text-2xl">
+      <span className="tooltip tooltip-top" data-tooltip="Back">
+        <FaBackwardStep
+          className="opacity-80 hover:opacity-100"
+          onClick={prevSong}
+        />
+      </span>
+      <span className="tooltip tooltip-top" data-tooltip="Play">
+        <PlayButton
+          onClick={playSong}
+          isPlaying={playing}
+          disabled={isLoading}
+          className="h-8 w-8 text-xs btn-secondary cursor-default"
+        />
+      </span>
+      <span className="tooltip tooltip-top" data-tooltip="Next">
+        <FaForwardStep
+          className="opacity-80 hover:opacity-100"
+          onClick={nextSong}
+        />
+      </span>
+    </div>
+  );
+};
+
+export default PlayerControl;
