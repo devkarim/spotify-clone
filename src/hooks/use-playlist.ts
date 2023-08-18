57b076f7012b26c63c4d5487b7d99df0d62dcd69
Karim <devkarim@hotmail.com>
2023-08-18 17:38:37 +0300

    Add "use playlist" hook
    Add "use playlist" hook that handles fetching current playing playlist

---
+++ src/hooks/use-playlist.ts
@@ -0,0 +1,32 @@
+import { create } from 'zustand';
+import { devtools } from 'zustand/middleware';
+
+import { FullPlaylist } from '@/types/db';
+import { getUserPlaylist } from '@/services/client/playlist';
+
+interface PlaylistState {
+  isLoading: boolean;
+  fetched: boolean;
+  playlist?: FullPlaylist;
+  fetch: (playlistId: bigint) => Promise<void>;
+  refresh: () => Promise<void>;
+}
+
+const usePlaylist = create<PlaylistState>()(
+  devtools((set, get) => ({
+    fetched: false,
+    isLoading: false,
+    fetch: async (playlistId) => {
+      set({ isLoading: true, fetched: false });
+      const playlist = await getUserPlaylist(playlistId);
+      set({ isLoading: false, fetched: true, playlist });
+    },
+    refresh: async () => {
+      const playlist = get().playlist;
+      if (!playlist) return;
+      await get().fetch(playlist.id);
+    },
+  }))
+);
+
+export default usePlaylist;
