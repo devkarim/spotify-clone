7359e7e83e746c7b882324abb4bf6602ba19a65d
Karim <devkarim@hotmail.com>
2023-08-18 17:54:26 +0300

    feat: edit main container gradient based on page
    Edit main container gradient color based on current page the user is in

--- src/components/ui/main-container.tsx
+++ src/components/ui/main-container.tsx
@@ -4,18 +4,22 @@ import { DetailedHTMLProps, HTMLAttributes } from 'react';
 
 import { cn } from '@/lib/utils';
 import useScroll from '@/hooks/use-scroll';
+import { usePathname } from 'next/navigation';
 
 interface MainContainerProps
   extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
   headerClassName?: string;
+  isSignedIn?: boolean;
 }
 
 const MainContainer: React.FC<MainContainerProps> = ({
   children,
   className,
   headerClassName,
+  isSignedIn,
   ...props
 }) => {
+  const pathname = usePathname();
   const scroll = useScroll();
 
   return (
@@ -27,7 +31,15 @@ const MainContainer: React.FC<MainContainerProps> = ({
       )}
       {...props}
     >
-      <div className={cn('absolute top-0 w-full h-64', headerClassName)} />
+      <div
+        className={cn(
+          'absolute top-0 w-full h-64',
+          isSignedIn && 'bg-gradient-to-b from-blue-800/40',
+          pathname.startsWith('/playlist') &&
+            'bg-gradient-to-b from-emerald-800/70',
+          headerClassName
+        )}
+      />
       <div className="relative">{children}</div>
     </div>
   );
