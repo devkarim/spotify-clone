c1e37dabc8f38e8182cab28e1956b4e765feb9f6
Karim <devkarim@hotmail.com>
2023-08-18 17:34:22 +0300

    Make clientside function for fetching playlist

--- src/services/client/playlist.ts
+++ src/services/client/playlist.ts
@@ -1,4 +1,5 @@
 import { Playlist } from '@prisma/client';
+import { FullPlaylist } from '@/types/db';
 import { BaseResponse } from '@/types/api';
 import { PlaylistSchema } from '@/schemas/playlistSchema';
 
@@ -6,5 +7,13 @@ import client from './axios';
 
 type PlaylistResponse = BaseResponse<Playlist>;
 
+type FullPlaylistResponse = BaseResponse<FullPlaylist>;
+
 export const createPlaylist = (data: PlaylistSchema) =>
   client.post<PlaylistResponse>('/playlist', data).then((res) => res.data.data);
+
+export const getUserPlaylist = async (playlistId: bigint) => {
+  return client
+    .get<FullPlaylistResponse>(`/playlist/${playlistId}`)
+    .then((res) => res.data.data);
+};
