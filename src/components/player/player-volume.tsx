fa42d7149c55bf3bad383020d4d416b68b26a1ed
Karim <devkarim@hotmail.com>
2023-08-18 18:13:37 +0300

    feat: add player volume component
    Add player volume component that controls main player volume by either
    up or down with ability to mute/unmute if pressed on volume icon.

---
+++ src/components/player/player-volume.tsx
@@ -0,0 +1,50 @@
+'use client';
+
+import { useGlobalAudioPlayer } from 'react-use-audio-player';
+import { FaVolumeLow, FaVolumeXmark, FaVolumeHigh } from 'react-icons/fa6';
+
+import usePlayer from '@/hooks/use-player';
+
+import PlayerRange from './player-range';
+
+interface PlayerVolumeProps {}
+
+const PlayerVolume: React.FC<PlayerVolumeProps> = () => {
+  const { mute, muted, volume, setVolume } = useGlobalAudioPlayer();
+  const setPlayerVolume = usePlayer((state) => state.setVolume);
+
+  return (
+    <div className="flex gap-2 self-center absolute right-6">
+      {
+        <span
+          className="tooltip tooltip-top"
+          data-tooltip={muted ? 'Unmute' : 'Mute'}
+        >
+          <div className="opacity-60 w-8" onClick={() => mute(!muted)}>
+            {!muted ? (
+              <>
+                {volume < 0.5 && volume != 0 && <FaVolumeLow />}
+                {volume == 0 && <FaVolumeXmark />}
+                {volume > 0.5 && <FaVolumeHigh />}
+              </>
+            ) : (
+              <FaVolumeXmark />
+            )}
+          </div>
+        </span>
+      }
+      <PlayerRange
+        value={volume}
+        max={1}
+        step={0.01}
+        className={muted ? 'opacity-60' : ''}
+        setValue={(newVolume) => {
+          setPlayerVolume(newVolume);
+          setVolume(newVolume);
+        }}
+      />
+    </div>
+  );
+};
+
+export default PlayerVolume;
