7e29bc17f08295eb57f330ef00378d9fbb8b4cdd
Karim <devkarim@hotmail.com>
2023-08-18 18:04:59 +0300

    feat: add player range component
    Add player range component that handles range inputs with ability to
    track change start and end to prevent instant change while user is
    grabbing the range input.

---
+++ src/components/player/player-range.tsx
@@ -0,0 +1,44 @@
+'use client';
+
+import { DetailedHTMLProps, InputHTMLAttributes } from 'react';
+
+import { cn } from '@/lib/utils';
+
+interface PlayerRangeProps
+  extends DetailedHTMLProps<
+    InputHTMLAttributes<HTMLInputElement>,
+    HTMLInputElement
+  > {
+  value: number;
+  setValue: (value: number) => void;
+  onChangeStart?: () => void;
+  onChangeDone?: (value: number) => void;
+}
+
+const PlayerRange: React.FC<PlayerRangeProps> = ({
+  value,
+  className,
+  onChangeStart,
+  onChangeDone,
+  setValue,
+  ...props
+}) => {
+  return (
+    <input
+      type="range"
+      className={cn(
+        'range range-secondary hover:range-primary w-full',
+        className
+      )}
+      value={value}
+      onChange={(e) => {
+        onChangeStart?.();
+        setValue(+e.target.value);
+      }}
+      onMouseUp={() => onChangeDone?.(value)}
+      {...props}
+    />
+  );
+};
+
+export default PlayerRange;
