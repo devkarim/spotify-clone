8d9c2b257fa4193da759d502ab9397ac3e56739d
Karim <devkarim@hotmail.com>
2023-08-19 23:25:15 +0300

    refactor: update item type to have song & playlist

--- src/types/ui.ts
+++ src/types/ui.ts
@@ -1,7 +1,11 @@
+import { Playlist, Song } from '@prisma/client';
+
 export interface Item {
   title: string;
   subtitle: string;
   imageUrl: string | null;
+  song?: Song;
+  playlist?: Playlist;
 }
 
 export type ModalStatus = 'create' | 'edit';
