610b6240f92ec69c0d3ecc4cc9a8a5280832e83b
Karim <devkarim@hotmail.com>
2023-08-20 03:48:40 +0300

    feat: make getUserPlaylists include count of songs

--- src/services/server/playlist.ts
+++ src/services/server/playlist.ts
@@ -20,6 +20,7 @@ export const getUserPlaylists = (userId?: bigint) =>
           userId,
         },
         orderBy: { updatedAt: 'desc' },
+        include: { _count: { select: { songs: true } } },
       })
     : [];
 
