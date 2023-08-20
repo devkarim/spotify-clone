35c10c3bec1b50546a0f7f65c875d5b8de149d06
Karim <devkarim@hotmail.com>
2023-08-20 03:21:49 +0300

    feat: show last played songs/playlists on home
    Display recently played songs and playlists on home page.

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,58 +1,58 @@
 import { getUser } from '@/services/server/session';
 import Section from '@/components/section/main-section';
 import WelcomeHeader from '@/components/ui/welcome-header';
-import { getUserPlaylists } from '@/services/server/playlist';
+import {
+  getLastPlayedPlaylists,
+  getUserPlaylists,
+} from '@/services/server/playlist';
 import HomePlaylists from '@/components/playlist/home-playlists';
+import { getLastPlayedSongs } from '@/services/server/song';
 
 interface HomePageProps {}
 
 const HomePage: React.FC<HomePageProps> = async ({}) => {
   const user = await getUser();
   const playlists = await getUserPlaylists(user?.id);
+  const lastPlayedPlaylists = await getLastPlayedPlaylists(user?.id);
+  const lastPlayedSongs = await getLastPlayedSongs(user?.id);
 
   const isSignedIn = user?.isAuthenticated;
 
-  return (
-    <>
-      {isSignedIn && (
+  if (isSignedIn) {
+    return (
+      <>
         <div className="p-6 space-y-6">
           <WelcomeHeader />
           <HomePlaylists playlists={playlists} />
         </div>
-      )}
-      <div className="p-6 pt-0 space-y-8">
-        {isSignedIn && (
+        <div className="p-6 pt-0 space-y-8">
           <Section
-            title="Recently Played"
-            items={playlists.map((p) => ({
-              title: p.name,
-              subtitle: 'Playlist - 0 songs',
-              imageUrl: p.imageUrl,
+            title="Songs recently played"
+            items={lastPlayedSongs.map((s) => ({
+              title: s.name,
+              subtitle: `Song`,
+              imageUrl: s.imageUrl,
+              song: s,
             }))}
           />
-        )}
-        {isSignedIn && (
           <Section
-            title="New picks"
-            items={playlists.map((p) => ({
+            title="Playlists recently played"
+            items={lastPlayedPlaylists.map((p) => ({
               title: p.name,
-              subtitle: 'Playlist - 0 songs',
+              subtitle: `Playlist - ${p._count.songs} songs`,
               imageUrl: p.imageUrl,
+              playlist: p,
             }))}
           />
-        )}
-        {isSignedIn && (
-          <Section
-            title="Welcome to 2023!"
-            items={playlists.map((p) => ({
-              title: p.name,
-              subtitle: 'Playlist - 0 songs',
-              imageUrl: p.imageUrl,
-            }))}
-          />
-        )}
-      </div>
-    </>
+        </div>
+      </>
+    );
+  }
+
+  return (
+    <div className="p-6 space-y-6">
+      <WelcomeHeader />
+    </div>
   );
 };
 
