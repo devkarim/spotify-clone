e887587629817ac2c1dcce2e725b15d0e4d1a5a9
Karim <devkarim@hotmail.com>
2023-08-20 03:20:44 +0300

    feat: redirect PlaylistHomeCard to playlist page
    Make PlaylistHomeCard redirect user to playlist page on click.

--- src/components/playlist/home-playlists.tsx
+++ src/components/playlist/home-playlists.tsx
@@ -1,4 +1,9 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+
 import { Playlist } from '@prisma/client';
+
 import PlaylistHomeCard from './playlist-home-card';
 
 interface HomePlaylistsProps {
@@ -6,10 +11,22 @@ interface HomePlaylistsProps {
 }
 
 const HomePlaylists: React.FC<HomePlaylistsProps> = ({ playlists = [] }) => {
+  const router = useRouter();
+
+  const onParentClick = (playlistId: bigint) => {
+    router.push(`/playlist/${playlistId}`);
+  };
+
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-6">
       {playlists.map((p) => (
-        <PlaylistHomeCard key={p.name} name={p.name} imageUrl={p.imageUrl} />
+        <PlaylistHomeCard
+          key={p.name}
+          playlistId={p.id}
+          name={p.name}
+          imageUrl={p.imageUrl}
+          onParentClick={() => onParentClick(p.id)}
+        />
       ))}
     </div>
   );
