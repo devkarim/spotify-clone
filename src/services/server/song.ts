d9b4a8dbe405b5cde718dca81c1460395c293947
Karim <devkarim@hotmail.com>
2023-08-20 03:02:47 +0300

    feat: add song last played functions serverside
    Add fetch and update last played songs functions.

--- src/services/server/song.ts
+++ src/services/server/song.ts
@@ -3,3 +3,22 @@ import { SongSchema } from '@/schemas/songSchema';
 
 export const createSong = (playlistId: bigint, songData: SongSchema) =>
   prisma.song.create({ data: { playlistId, ...songData } });
+
+export const getSongById = (id: bigint) =>
+  prisma.song.findUnique({ where: { id }, include: { playlist: true } });
+
+export const updateSongLastPlayed = (id: bigint) =>
+  prisma.song.update({
+    where: { id },
+    data: { lastPlayedAt: new Date() },
+  });
+
+export const getLastPlayedSongs = (userId?: bigint) =>
+  userId
+    ? prisma.song.findMany({
+        where: { playlist: { userId } },
+        include: { playlist: true },
+        orderBy: { lastPlayedAt: 'desc' },
+        take: 10,
+      })
+    : [];
