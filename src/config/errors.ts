b3853be201ff346eff0f71f2f1a432195245ebb3
Karim <devkarim@hotmail.com>
2023-08-20 03:01:11 +0300

    refactor: add more error messages
    Add error messages for unauthorized and invalid song id.

--- src/config/errors.ts
+++ src/config/errors.ts
@@ -4,8 +4,10 @@ import Messages from './messages';
 const Errors = {
   invalidCredentials: ServerError.new(Messages.errors.invalidCredentials, 400),
   emailTaken: ServerError.new(Messages.errors.emailTaken, 400),
-  unauthenticated: ServerError.new(Messages.errors.unauthenticated, 400),
+  unauthenticated: ServerError.new(Messages.errors.unauthenticated, 401),
+  unauthorized: ServerError.new(Messages.errors.unauthorized, 403),
   invalidPlaylistId: ServerError.new(Messages.errors.invalidPlaylistId, 400),
+  invalidSongId: ServerError.new(Messages.errors.invalidSongId, 400),
   invalidId: ServerError.new(Messages.errors.invalidId, 400),
   notFound: ServerError.new(Messages.errors.notFound, 404),
   invalidFile: ServerError.new(Messages.errors.invalidFile, 404),
