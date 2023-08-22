dd49fe981648c4b2bfabf5aea52698e2b2f58eb9
Karim <devkarim@hotmail.com>
2023-08-22 04:04:09 +0300

    feat: add api route for deleting songs
    Add an API route for deleting songs by ID.

--- src/app/api/song/[songId]/route.ts
+++ src/app/api/song/[songId]/route.ts
@@ -2,9 +2,9 @@ import log from '@/lib/log';
 import Errors from '@/config/errors';
 import Response from '@/types/server';
 import { toBigIntSafe } from '@/lib/utils';
-import { getUser } from '@/services/server/session';
-import { editSong } from '@/services/server/song';
 import { songSchema } from '@/schemas/songSchema';
+import { getUser } from '@/services/server/session';
+import { editSong, deleteSong } from '@/services/server/song';
 
 export async function PATCH(
   req: Request,
@@ -24,6 +24,26 @@ export async function PATCH(
     return Response.success(song);
   } catch (err) {
     log.error(err, 'patch /api/song/[id]');
+    return Response.error(err);
+  }
+}
+
+export async function DELETE(
+  req: Request,
+  { params }: { params: { songId: string } }
+) {
+  try {
+    const user = await getUser();
+    if (!user) {
+      throw Errors.unauthenticated;
+    }
+    const songId = toBigIntSafe(params.songId);
+    if (!songId) throw Errors.invalidSongId;
+    const song = await deleteSong(songId, user.id);
+    if (!song) throw Errors.invalidSongId;
+    return Response.success();
+  } catch (err) {
+    log.error(err, 'delete /api/song/[id]');
     return Response.error(err);
   }
 }
