655e01a9818413ca73c4777ced49a0439e551090
Karim <devkarim@hotmail.com>
2023-08-22 03:29:42 +0300

    feat: add "song" to song modal state
    Add "song" to song modal state for editing on modal open.

--- src/hooks/use-song-modal.tsx
+++ src/hooks/use-song-modal.tsx
@@ -1,23 +1,27 @@
 import { create } from 'zustand';
 import { devtools } from 'zustand/middleware';
 
+import { Song } from '@prisma/client';
+
 import { ModalStatus } from '@/types/ui';
 
 interface SongModalState {
   isOpen: boolean;
   status: ModalStatus;
+  song: Song | null;
   playlistId?: bigint;
-  show: (status?: ModalStatus, playlistId?: bigint) => void;
+  show: (status?: ModalStatus, playlistId?: bigint, song?: Song) => void;
   hide: () => void;
 }
 
 const useSongModal = create<SongModalState>()(
   devtools((set) => ({
     status: 'create',
+    song: null,
     isOpen: false,
-    show: (status = 'create', playlistId) =>
-      set({ status, playlistId, isOpen: true }),
-    hide: () => set({ isOpen: false }),
+    show: (status = 'create', playlistId, song) =>
+      set({ status, playlistId, song, isOpen: true }),
+    hide: () => set({ isOpen: false, song: null }),
   }))
 );
 
