d6ea43d33deff33a4befcebc996a54ababeaaeca
Karim <devkarim@hotmail.com>
2023-08-22 03:26:09 +0300

    feat: make actions dropdown to update/delete
    Make actions dropdown component with actions: update & delete.

---
+++ src/components/ui/actions-dropdown.tsx
@@ -0,0 +1,41 @@
+import { FaEdit, FaEllipsisH, FaTrash } from 'react-icons/fa';
+
+interface ActionsDropdownProps {
+  onDelete: () => void;
+  onUpdate: () => void;
+}
+
+const ActionsDropdown: React.FC<ActionsDropdownProps> = ({
+  onDelete,
+  onUpdate,
+}) => {
+  return (
+    <div>
+      <div className="dropdown">
+        <label className="h-full cursor-pointer" tabIndex={0}>
+          <FaEllipsisH className="text-lg ml-2" />
+        </label>
+        <div className="dropdown-menu dropdown-menu-left z-[10] w-40">
+          <div
+            tabIndex={1}
+            className="dropdown-item flex-row gap-2 text-sm"
+            onClick={onUpdate}
+          >
+            <FaEdit />
+            <p>Update</p>
+          </div>
+          <div
+            tabIndex={2}
+            className="dropdown-item flex-row gap-2 text-sm"
+            onClick={onDelete}
+          >
+            <FaTrash />
+            <p>Delete</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ActionsDropdown;
