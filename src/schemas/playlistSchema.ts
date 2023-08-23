bb000aeb7b6d185c2e94929beccdf9300c67c0d5
Karim <devkarim@hotmail.com>
2023-08-24 01:50:10 +0300

    fix: make imageUrl in schema optional

--- src/schemas/playlistSchema.ts
+++ src/schemas/playlistSchema.ts
@@ -4,7 +4,11 @@ export const playlistSchema = z.object({
   name: z
     .string({ required_error: 'Playlist name is required' })
     .min(1, 'Playlist name is required'),
-  imageUrl: z.string().url('Playlist image is invalid').optional(),
+  imageUrl: z
+    .string()
+    .url('Playlist image is invalid')
+    .optional()
+    .or(z.undefined()),
 });
 
 export type PlaylistSchema = z.infer<typeof playlistSchema>;
