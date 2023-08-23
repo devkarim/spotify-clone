8a3bff02eab3d922ebad8fa373d50a62d795a1d3
Karim <devkarim@hotmail.com>
2023-08-24 01:49:06 +0300

    feat: add rpi route for updating playlist

--- src/app/api/playlist/[playlistId]/route.ts
+++ src/app/api/playlist/[playlistId]/route.ts
@@ -3,7 +3,8 @@ import Errors from '@/config/errors';
 import Response from '@/types/server';
 import { toBigIntSafe } from '@/lib/utils';
 import { getUser } from '@/services/server/session';
-import { getUserPlaylist } from '@/services/server/playlist';
+import { playlistSchema } from '@/schemas/playlistSchema';
+import { getUserPlaylist, updatePlaylist } from '@/services/server/playlist';
 
 export async function GET(
   req: Request,
@@ -17,6 +18,27 @@ export async function GET(
     const playlistId = toBigIntSafe(params.playlistId);
     if (!playlistId) throw Errors.invalidPlaylistId;
     const playlist = await getUserPlaylist(user.id, playlistId);
+    return Response.success(playlist);
+  } catch (err) {
+    log.error(err, 'get /api/playlist/[id]');
+    return Response.error(err);
+  }
+}
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { playlistId: string } }
+) {
+  const body = await req.json();
+  try {
+    const user = await getUser();
+    if (!user) {
+      throw Errors.unauthenticated;
+    }
+    const playlistId = toBigIntSafe(params.playlistId);
+    if (!playlistId) throw Errors.invalidPlaylistId;
+    const data = playlistSchema.parse(body);
+    const playlist = await updatePlaylist(user.id, playlistId, data);
     return Response.success(playlist);
   } catch (err) {
     log.error(err, 'get /api/playlist/[id]');
