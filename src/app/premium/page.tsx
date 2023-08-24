7813fb5225635c9c8dacc2bddae0fb1d750746f3
Karim <devkarim@hotmail.com>
2023-08-24 04:03:44 +0300

    feat: setup premium page

---
+++ src/app/premium/page.tsx
@@ -0,0 +1,9 @@
+import Container from '@/components/ui/container';
+
+interface PremiumPageProps {}
+
+const PremiumPage: React.FC<PremiumPageProps> = ({}) => {
+  return <Container>Premium Page</Container>;
+};
+
+export default PremiumPage;
