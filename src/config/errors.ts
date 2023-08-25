f445cceb1a18461e1436fb5b203029b2a8951dfe
Karim <devkarim@hotmail.com>
2023-08-25 05:49:30 +0300

    feat: add error msgs including limits & premium
    Add new error messages including max songs/playlists limit and if user
    is already premium.

--- src/config/errors.ts
+++ src/config/errors.ts
@@ -8,10 +8,13 @@ const Errors = {
   invalidSongId: ServerError.new(Messages.errors.invalidSongId, 400),
   invalidPlaylistId: ServerError.new(Messages.errors.invalidPlaylistId, 400),
   invalidCredentials: ServerError.new(Messages.errors.invalidCredentials, 400),
+  invalidFile: ServerError.new(Messages.errors.invalidFile, 400),
   unauthenticated: ServerError.new(Messages.errors.unauthenticated, 401),
   unauthorized: ServerError.new(Messages.errors.unauthorized, 403),
+  maxSongs: ServerError.new(Messages.errors.maxSongs, 403),
+  maxPlaylists: ServerError.new(Messages.errors.maxPlaylists, 403),
   notFound: ServerError.new(Messages.errors.notFound, 404),
-  invalidFile: ServerError.new(Messages.errors.invalidFile, 404),
+  alreadyPremium: ServerError.new(Messages.errors.alreadyPremium, 409),
 } as const;
 
 export default Errors;
--- src/config/messages.ts
+++ src/config/messages.ts
@@ -2,14 +2,17 @@ const Messages = {
   errors: {
     invalidCredentials: 'Invalid email or password.',
     emailTaken: 'Email is already taken.',
-    unauthenticated: 'Unauthenticated.',
-    unauthorized: "You don't have permission to do that.",
     invalidPlaylistId: 'Invalid playlist id.',
     invalidSongId: 'Invalid song id.',
-    invalidId: 'Invalid id.',
-    notFound: 'Not found.',
-    invalidFile: 'Invalid file.',
     queryRequired: 'Search query is required.',
+    invalidFile: 'Invalid file.',
+    invalidId: 'Invalid id.',
+    unauthenticated: 'Unauthenticated.',
+    unauthorized: "You don't have permission to do that.",
+    maxSongs: 'Max songs reached. Subscribe to premium plan.',
+    maxPlaylists: 'Max playlists reached. Subscribe to premium plan.',
+    notFound: 'Not found.',
+    alreadyPremium: 'Already in premium plan.',
   },
 } as const;
 
