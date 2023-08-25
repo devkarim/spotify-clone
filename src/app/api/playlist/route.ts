e17d761e63221ad02a4384df53768401b36a9332
Karim <devkarim@hotmail.com>
2023-08-25 06:34:02 +0300

    feat: check user's playlists before creating one
    Check user's playlists count before creating a new playlist. Throw error
    if the count exceeds maximum limit.

--- src/app/api/playlist/route.ts
+++ src/app/api/playlist/route.ts
@@ -4,14 +4,18 @@ import Response from '@/types/server';
 import { getUser } from '@/services/server/session';
 import { playlistSchema } from '@/schemas/playlistSchema';
 import { createPlaylist } from '@/services/server/playlist';
+import { getUserById } from '@/services/server/user';
+import { MAX_PLAYLISTS_PER_USER } from '@/config/limits';
 
 export async function POST(req: Request) {
   const body = await req.json();
   try {
     const user = await getUser();
-    if (!user) {
-      throw Errors.unauthenticated;
-    }
+    if (!user) throw Errors.unauthenticated;
+    const fullUser = await getUserById(user.id);
+    if (!fullUser) throw Errors.invalidId;
+    if (!user.isPremium && fullUser.playlists.length >= MAX_PLAYLISTS_PER_USER)
+      throw Errors.maxPlaylists;
     const { name, imageUrl } = playlistSchema.parse(body);
     const playlist = await createPlaylist(user.id, name, imageUrl);
     return Response.success(playlist);
