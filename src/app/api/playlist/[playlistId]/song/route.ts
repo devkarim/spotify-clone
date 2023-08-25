f7e494c43c125f19ef176192335ec20e5b5aef42
Karim <devkarim@hotmail.com>
2023-08-25 06:34:21 +0300

    feat: check user's songs count before creating one
    Check user's songs count before creating a new song. Throw error
    if the count exceeds maximum limit.

--- src/app/api/playlist/[playlistId]/song/route.ts
+++ src/app/api/playlist/[playlistId]/song/route.ts
@@ -5,6 +5,8 @@ import { toBigInt } from '@/lib/utils';
 import { songSchema } from '@/schemas/songSchema';
 import { createSong } from '@/services/server/song';
 import { getUser } from '@/services/server/session';
+import { MAX_SONGS_PER_PLAYLIST } from '@/config/limits';
+import { getPlaylistById } from '@/services/server/playlist';
 
 export async function POST(
   req: Request,
@@ -14,9 +16,11 @@ export async function POST(
   try {
     const playlistId = toBigInt(params.playlistId);
     const user = await getUser();
-    if (!user) {
-      throw Errors.unauthenticated;
-    }
+    if (!user) throw Errors.unauthenticated;
+    const playlist = await getPlaylistById(playlistId);
+    if (!playlist) throw Errors.invalidPlaylistId;
+    if (!user.isPremium && playlist.songs.length >= MAX_SONGS_PER_PLAYLIST)
+      throw Errors.maxSongs;
     const songData = songSchema.parse(body);
     const song = await createSong(playlistId, songData);
     return Response.success(song);
