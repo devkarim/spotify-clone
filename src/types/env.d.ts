c2651001214384d56e4e36a6fbda3a33128008e7
Karim <devkarim@hotmail.com>
2023-08-25 05:43:47 +0300

    refactor: update env variables with stripe secrets

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -2,6 +2,9 @@ namespace NodeJS {
   interface ProcessEnv {
     GITHUB_CLIENT_ID: string;
     GITHUB_CLIENT_SECRET: string;
-    FAWATERK_API_KEY: string;
+    NEXT_PUBLIC_STRIPE_PUBLIC_KEY: string;
+    STRIPE_SECRET_KEY: string;
+    STRIPE_WEBHOOK_SECRET: string;
+    STRIPE_PREMIUM_PLAN_ID: string;
   }
 }
