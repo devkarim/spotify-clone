034fe789654c8edf9eba90fdeb4d105f6995c588
Karim <devkarim@hotmail.com>
2023-08-25 05:52:01 +0300

    feat: add api route for deleting playlists
    Add an API route for deleting playlists based on playlist ID and user
    session.

--- src/app/api/playlist/[playlistId]/route.ts
+++ src/app/api/playlist/[playlistId]/route.ts
@@ -4,7 +4,11 @@ import Response from '@/types/server';
 import { toBigIntSafe } from '@/lib/utils';
 import { getUser } from '@/services/server/session';
 import { playlistSchema } from '@/schemas/playlistSchema';
-import { getUserPlaylist, updatePlaylist } from '@/services/server/playlist';
+import {
+  getUserPlaylist,
+  updatePlaylist,
+  deletePlaylist,
+} from '@/services/server/playlist';
 
 export async function GET(
   req: Request,
@@ -39,9 +43,29 @@ export async function PATCH(
     if (!playlistId) throw Errors.invalidPlaylistId;
     const data = playlistSchema.parse(body);
     const playlist = await updatePlaylist(user.id, playlistId, data);
+    if (!playlist) throw Errors.invalidPlaylistId;
     return Response.success(playlist);
   } catch (err) {
     log.error(err, 'patch /api/playlist/[id]');
+    return Response.error(err);
+  }
+}
+
+export async function DELETE(
+  req: Request,
+  { params }: { params: { playlistId: string } }
+) {
+  try {
+    const user = await getUser();
+    if (!user) throw Errors.unauthenticated;
+
+    const playlistId = toBigIntSafe(params.playlistId);
+    if (!playlistId) throw Errors.invalidPlaylistId;
+    const playlist = await deletePlaylist(user.id, playlistId);
+    if (!playlist) throw Errors.invalidPlaylistId;
+    return Response.success(playlist);
+  } catch (err) {
+    log.error(err, 'delete /api/playlist/[id]');
     return Response.error(err);
   }
 }
