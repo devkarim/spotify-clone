6d5359752719c5afdf2877838a214ed7afee8502
Karim <devkarim@hotmail.com>
2023-08-25 07:03:58 +0300

    fix: make music-no-image component not selectable

--- src/components/ui/music-no-image.tsx
+++ src/components/ui/music-no-image.tsx
@@ -1,4 +1,5 @@
-import { FiMusic } from 'react-icons/fi';
+// import { FiMusic } from 'react-icons/fi';
+import { FiMusic } from '@react-icons/all-files/fi/FiMusic';
 
 import { cn } from '@/lib/utils';
 
@@ -8,7 +9,7 @@ interface MusicNoImageProps {
 
 const MusicNoImage: React.FC<MusicNoImageProps> = ({ className }) => {
   return (
-    <div className="flex justify-center items-center w-full h-full bg-section-card">
+    <div className="flex justify-center items-center w-full h-full bg-section-card select-none">
       <FiMusic className={cn('text-7xl opacity-60', className)} />
     </div>
   );
