505091c5f67ac0331cf2108aeb6bb07336fdbf21
Karim <devkarim@hotmail.com>
2023-08-25 06:40:51 +0300

    feat: design premium page
    Design premium page to include current plan and a subscribe button.

--- src/app/premium/page.tsx
+++ src/app/premium/page.tsx
@@ -1,9 +1,34 @@
+import Errors from '@/config/errors';
+import { APP_NAME } from '@/config/constants';
 import Container from '@/components/ui/container';
+import { getUser } from '@/services/server/session';
+import SubscribeButton from '@/components/subscription/subscribe-button';
 
 interface PremiumPageProps {}
 
-const PremiumPage: React.FC<PremiumPageProps> = ({}) => {
-  return <Container>Premium Page</Container>;
+const PremiumPage: React.FC<PremiumPageProps> = async ({}) => {
+  const user = await getUser();
+
+  if (!user) throw Errors.unauthenticated;
+
+  const isSubscribed = user.isPremium;
+
+  return (
+    <Container className="space-y-8">
+      <h2 className="font-bold text-5xl">Premium</h2>
+      {isSubscribed ? (
+        <p>
+          You are currently subscribed to{' '}
+          <span className="font-bold">{APP_NAME} Premium Plan</span> plan.
+        </p>
+      ) : (
+        <p>
+          You are not subscribed to any <span className="font-bold">plan</span>.
+        </p>
+      )}
+      <SubscribeButton isSubscribed={isSubscribed} />
+    </Container>
+  );
 };
 
 export default PremiumPage;
