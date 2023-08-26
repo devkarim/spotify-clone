288552d0e30aaf8eb5584267bd0243d26816e92c
Karim <devkarim@hotmail.com>
2023-08-26 03:51:17 +0300

    feat: add constants for project author details

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,6 +1,9 @@
 export const APP_NAME = 'Spotify Clone';
 export const APP_VERSION = '1.0.0';
 
+export const AUTHOR_NAME = 'devkarim';
+export const AUTHOR_EMAIL = 'devkarim@hotmail.com';
+
 export const isDevelopment = process.env.NODE_ENV === 'development';
 export const isProduction = process.env.NODE_ENV === 'production';
 
