b5de743c8815ca913d4d2dc1ceef241c91c8ce4a
Karim <devkarim@hotmail.com>
2023-08-26 04:00:07 +0300

    feat: add message when guest visits premium page
    Add "log in or create new account" msg when a guest visits premium page.

--- src/app/premium/page.tsx
+++ src/app/premium/page.tsx
@@ -1,4 +1,3 @@
-import Errors from '@/config/errors';
 import { APP_NAME } from '@/config/constants';
 import Container from '@/components/ui/container';
 import { getUser } from '@/services/server/session';
@@ -9,13 +8,22 @@ interface PremiumPageProps {}
 const PremiumPage: React.FC<PremiumPageProps> = async ({}) => {
   const user = await getUser();
 
-  if (!user) throw Errors.unauthenticated;
+  if (!user)
+    return (
+      <Container className="space-y-8">
+        <h2 className="font-bold text-5xl">{APP_NAME} Premium</h2>
+        <p className="opacity-60">
+          To subscribe to our premium plan, please log in or create a new
+          account.
+        </p>
+      </Container>
+    );
 
   const isSubscribed = user.isPremium;
 
   return (
     <Container className="space-y-8">
-      <h2 className="font-bold text-5xl">Premium</h2>
+      <h2 className="font-bold text-5xl">{APP_NAME} Premium</h2>
       {isSubscribed ? (
         <p>
           You are currently subscribed to{' '}
