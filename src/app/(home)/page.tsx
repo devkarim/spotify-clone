6091263ede8f4ec68720a4261198d8b83b59757b
Karim <devkarim@hotmail.com>
2023-08-26 04:00:55 +0300

    feat: design homepage for guests
    Design homepage for guests that displays website's features and a link
    to about us page.

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,3 +1,10 @@
+import Link from 'next/link';
+import { FiMusic } from '@react-icons/all-files/fi/FiMusic';
+import { FaPlay } from '@react-icons/all-files/fa/FaPlay';
+import { FaSearch } from '@react-icons/all-files/fa/FaSearch';
+import { FaArrowRight } from '@react-icons/all-files/fa/FaArrowRight';
+import { PiPlaylistBold } from '@react-icons/all-files/pi/PiPlaylistBold';
+
 import { getUser } from '@/services/server/session';
 import Section from '@/components/section/main-section';
 import WelcomeHeader from '@/components/ui/welcome-header';
@@ -5,8 +12,11 @@ import {
   getLastPlayedPlaylists,
   getUserPlaylists,
 } from '@/services/server/playlist';
-import HomePlaylists from '@/components/playlist/home-playlists';
 import { getLastPlayedSongs } from '@/services/server/song';
+import HomePlaylists from '@/components/playlist/home-playlists';
+
+import FeatureCard from './components/feature-card';
+import RegisterButton from '@/components/ui/register-button';
 
 interface HomePageProps {}
 
@@ -50,8 +60,72 @@ const HomePage: React.FC<HomePageProps> = async ({}) => {
   }
 
   return (
-    <div className="p-6 space-y-6">
-      <WelcomeHeader />
+    <div className="p-6 space-y-12">
+      <div className="space-y-3">
+        <WelcomeHeader />
+        <p className="opacity-60">
+          A web music player where you can add your favorite songs to your
+          customized playlists.
+        </p>
+      </div>
+      <div className="flex flex-wrap w-full flex-col sm:flex-row items-center sm:items-stretch sm:justify-normal gap-6">
+        <FeatureCard
+          icon={<PiPlaylistBold />}
+          title="Manage Your Playlists"
+          subtitle="Add customized playlists to your library."
+        />
+        <FeatureCard
+          icon={<FiMusic />}
+          title="Songs Management"
+          subtitle="Add songs to your customized playlists."
+        />
+        <FeatureCard
+          icon={<FaPlay className="text-xl" />}
+          title="Global Music Player"
+          subtitle="Play your playlists and songs in a global music player."
+        />
+        <FeatureCard
+          icon={<FaSearch className="text-2xl" />}
+          title="Search Your Songs"
+          subtitle="Search any song in all of your playlists at once."
+        />
+        <FeatureCard
+          icon={<FaSearch className="text-2xl" />}
+          title="Recent Songs"
+          subtitle="See your recently played songs and playlists."
+        />
+        <FeatureCard
+          icon={<FaSearch className="text-2xl" />}
+          title="Customize Your Songs"
+          subtitle="Change the name and image of your songs with ease."
+        />
+      </div>
+      <div className="space-y-8">
+        <div className="space-y-3">
+          <h1 className="text-3xl lg:text-4xl font-bold">
+            Are you ready to join the journey?
+          </h1>
+          <p className="opacity-60">
+            Create an account now for free and enjoy listening to your favorite
+            songs wherever you are!
+          </p>
+        </div>
+        <RegisterButton
+          title="Sign up"
+          className="btn w-56 btn-secondary opacity-100"
+        />
+      </div>
+      <div className="space-y-8">
+        <div className="space-y-3">
+          <Link
+            href="/about"
+            className="text-xl lg:text-2xl font-bold underline flex gap-3 items-center w-fit"
+          >
+            <p>About Us</p>
+            <FaArrowRight className="text-2xl" />
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
