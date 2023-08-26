8f9e5b4bc96d403aacfe3919348ad295f53098b4
Karim <devkarim@hotmail.com>
2023-08-26 04:43:31 +0300

    refactor: change title and description of metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,19 +4,21 @@ import 'react-toastify/dist/ReactToastify.css';
 import type { Metadata } from 'next';
 import { Figtree } from 'next/font/google';
 
-import SidebarContent from '@/components/sidebar/sidebar-content';
+import { APP_NAME } from '@/config/constants';
+import SongModal from '@/components/modals/song-modal';
 import LoginModal from '@/components/modals/login-modal';
 import PlaylistModal from '@/components/modals/playlist-modal';
 import AuthProvider from '@/components/providers/auth-provider';
 import ToastProvider from '@/components/providers/toast-provider';
-import SongModal from '@/components/modals/song-modal';
+import SidebarContent from '@/components/sidebar/sidebar-content';
 import SubscriptionaModal from '@/components/modals/subscription-modal';
 
 const font = Figtree({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Spotify - Listen to Music wherever you are',
-  description: 'Web music player to listen to your favorite songs.',
+  title: `${APP_NAME} - Listen to Music everywhere`,
+  description:
+    'A web music player where you can listen & add your favorite songs to your customized playlists.',
 };
 
 export default async function RootLayout({
